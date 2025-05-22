3687c9aba890f54143fd8d38067e67afe26534e7
omarebid1 <omar3bid18@gmail.com>
2025-05-22 14:04:59 +0300

    add ItemRepository interface and SecurityConfig class

---
+++ todo-service/src/main/java/com/todoservice/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.todoservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+
+}
---
+++ todo-service/src/main/java/com/todoservice/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.todoservice.repository;
+
+import com.todoservice.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Optional<Item> findByTitle(String title);
+
+}
