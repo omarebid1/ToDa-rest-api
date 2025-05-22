b3aa5527ac93d14a8faf977adfe14bdd6c3e2a4b
omarebid1 <omar3bid18@gmail.com>
2025-05-22 14:16:00 +0300

    add validation annotations to ItemRequest and ItemDetailsRequest DTOs

--- todo-service/src/main/java/com/todoservice/dto/request/ItemDetailsRequest.java
+++ todo-service/src/main/java/com/todoservice/dto/request/ItemDetailsRequest.java
@@ -1,13 +1,17 @@
 package com.todoservice.dto.request;
 
 import com.todoservice.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ItemDetailsRequest {
 
+    @NotBlank(message = "Description is required")
     private String description;
+    @NotBlank(message = "Status is required")
     private Boolean status;
+    @NotBlank(message = "Priority is required")
     private Priority priority;
 
 }
--- todo-service/src/main/java/com/todoservice/dto/request/ItemRequest.java
+++ todo-service/src/main/java/com/todoservice/dto/request/ItemRequest.java
@@ -1,12 +1,16 @@
 package com.todoservice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ItemRequest {
 
+    @NotBlank(message = "Title is required")
     private String title;
+    @NotBlank(message = "User Id is required")
     private Long userId;
+    @NotBlank(message = "Item Details is required")
     private ItemDetailsRequest itemDetails;
 
 }
