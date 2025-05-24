b1b0f2e7bb97e53abc165132e9e43151b07a17e3
omarebid1 <omar3bid18@gmail.com>
2025-05-24 12:52:18 +0300

    add Role enum and update User class to implement role-based access control

---
+++ user-service/src/main/java/com/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.userservice.repository;
+
+import com.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findUserByEmail(String email);
+
+}
---
+++ user-service/src/main/java/com/userservice/service/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.userservice.service;
+
+import com.userservice.exception.UserNotFoundEx;
+import com.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UserNotFoundEx {
+        return userRepository.findUserByEmail(email);
+    }
+
+}
