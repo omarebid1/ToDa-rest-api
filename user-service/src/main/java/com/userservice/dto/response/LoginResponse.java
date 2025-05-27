ad2b0401a7ada149d439d74318a4a5fe17205b18
omarebid1 <omar3bid18@gmail.com>
2025-05-27 18:07:07 +0300

    add LoginResponse class and update application.yml for JWT configuration

---
+++ user-service/src/main/java/com/userservice/dto/response/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.userservice.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String message;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime timestamp;
+    private String token;
+
+}
